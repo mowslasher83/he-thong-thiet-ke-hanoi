017805f414e66c3f02f439c6d51f2ce8791e028c
mowslasher83 <radeswaled@hotmail.com>
2025-11-04 05:12:45 -0600

    Add comments to babel.config.js for clarity.

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = {
+  // Use the latest JavaScript features
   presets: [
     '@babel/preset-env',
+    // Transform JSX syntax for React
     '@babel/preset-react',
+    // Provide TypeScript support
     '@babel/preset-typescript'
   ]
 };
