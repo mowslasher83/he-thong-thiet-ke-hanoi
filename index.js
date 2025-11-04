41bee4089069cc6377166fd09a5de5ee22f8b98d
mowslasher83 <radeswaled@hotmail.com>
2025-11-04 05:12:23 -0600

    Update welcome message for clarity.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div>
       <h1>Hệ Thống Thiết Kế</h1>
-      <p>Chào mừng bạn đến với hệ thống thiết kế!</p>
+      <p>Chào mừng bạn đến với hệ thống thiết kế của chúng tôi!</p>
     </div>
   );
 };
