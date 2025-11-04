db1eaf11e717eb334b5b6c89a497dfc618592cd2
mowslasher83 <radeswaled@hotmail.com>
2025-11-04 05:10:23 -0600

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hệ Thiết Kế Hà Nội</h1>
+      <p>Chào mừng bạn đến với hệ thống thiết kế!</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
